a9379be4f34c1ba36e460ef981395fd2ec97cae7
Perry <puyepingx@gmail.com>
2024-09-04 19:37:17 +0900

    first commit

---
+++ WednesdayTest/Controllers/FirstTabVC.swift
@@ -0,0 +1,22 @@
+//
+//  FirstTabVC.swift
+//  WednesdayTest
+//
+//  Created by Perry Perrin on R 6/09/04.
+//
+
+import UIKit
+
+class FirstTabVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    @IBAction func NextPageBtn(_ sender: UIButton) {
+        print("next page button tapped...")
+    }
+    
+}
---
+++ WednesdayTest/Controllers/ThirdTabVC.swift
@@ -0,0 +1,29 @@
+//
+//  ThirdTabVC.swift
+//  WednesdayTest
+//
+//  Created by Perry Perrin on R 6/09/04.
+//
+
+import UIKit
+
+class ThirdTabVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
