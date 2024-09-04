7ea32b6374096ab62eeb9ee5d9fb0d20b69e5b71
Perry <puyepingx@gmail.com>
2024-09-04 19:25:03 +0900

    Initial Commit

---
+++ WednesdayTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WednesdayTest
+//
+//  Created by Perry Perrin on R 6/09/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
